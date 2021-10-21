7501a92f0c0ce1b6a3f6fcef9b264a123a315464
Adam Kruschwitz <a.j.kruschwitz@gmail.com>
2021-10-21 11:16:06 -0700

    added post route for api/notes

--- routes/api.js
+++ routes/api.js
@@ -6,12 +6,35 @@ const err = (er) => {
     er ? console.log("process failed: ", er) : console.log("process finished");
 }
 
-api.get('/notes', (req, res) => {
-    fs.readFile('./db/db.json', (er, data) => {
+// File reading helper function
+function readFile(filename, cb) {
+    fs.readFile(filename, (er, data) => {
         err(er); // record scratch sound effect as code
         data = JSON.parse(data);
-        res.json(data);
+        cb(data);
+    });
+}
+
+// File writing helper function
+function writeFile(filename, data) {
+    fs.writeFile(filename, JSON.stringify(data), err);
+}
+
+api.get('/notes', (req, res) => {
+    readFile('./db/db.json', data => { res.json(data) })
+});
+
+// POST request for creating a new note
+api.post('/notes', (req, res) => {
+    readFile('./db/db.json', data => {
+        // TODO: add unique ID.
+        // console.log(req.body);
+        data.push(req.body);
+        writeFile('./db/db.json', data);
+        res.send("Note saved successfully");
     });
 });
+
+
 
 module.exports = api;
