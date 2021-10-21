fd6dfa24e27278ae7d4acb60db0d191e28990e3f
Adam Kruschwitz <a.j.kruschwitz@gmail.com>
2021-10-21 10:26:25 -0700

    added get routes for index.html and notes.html

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const fs = require('fs');
+const path = require('path');
 
 const PORT = process.env.port || 3001;
 
@@ -10,3 +11,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
+});
